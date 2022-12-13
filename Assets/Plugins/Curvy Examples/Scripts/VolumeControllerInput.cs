a379c118efe3e44fb5eb2d73ff2b943c94445d35
kamranjamil343 <86221612+kamranjamil343@users.noreply.github.com>
2022-12-13 19:23:24 +0500

    update

--- Assets/Plugins/Curvy Examples/Scripts/VolumeControllerInput.cs
+++ Assets/Plugins/Curvy Examples/Scripts/VolumeControllerInput.cs
@@ -9,6 +9,7 @@ using UnityEngine;
 using System.Collections;
 using FluffyUnderware.Curvy.Controllers;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 namespace FluffyUnderware.Curvy.Examples
 {
@@ -21,10 +22,13 @@ namespace FluffyUnderware.Curvy.Examples
         public float maxSpeed = 40f;
         public float accelerationForward = 20f;
         public float accelerationBackward = 40f;
+        private float X_POS;
+        public float pixetDistToDetect;
         public bool mGameOver;
         private bool IsPlay;
         public Text text_s;
         public Vector3 mov_Val;
+        public Vector3 start_Pos;
         private void Awake()
         {
             if (!volumeController)
@@ -38,6 +42,40 @@ namespace FluffyUnderware.Curvy.Examples
             else
                 volumeController.OnInitialized.AddListener(arg0 => ResetController());
         }
+        public void UpAndDown(bool isTrue) 
+        {
+            start_Pos = Input.mousePosition;
+            if (isTrue)
+            {
+                start_Pos = Input.mousePosition;
+                IsPlay = true;
+                mov_Val = new Vector3(0, 1, 0);
+                volumeController.Speed = 40f;
+                print("Down");
+            }
+            else
+            {
+                IsPlay = false;
+                mov_Val = new Vector3(0, 0, 0);
+                X_POS = 0f;
+                StartCoroutine(SpeedTest());
+                print("Up");
+            }
+
+        }
+        public void Drag()
+        {
+            if (Input.mousePosition.x >= start_Pos.x + pixetDistToDetect)
+            {
+                print("right_Drag");
+                 X_POS = 0.7f;
+            }
+            else if (Input.mousePosition.x <= start_Pos.x + pixetDistToDetect)
+            {
+                 X_POS = -0.7f;
+                print("Left_Drag");
+            }
+        }
 
         private void ResetController()
         {
@@ -50,20 +88,16 @@ namespace FluffyUnderware.Curvy.Examples
         {
             if (!mGameOver)
             {
-                if (Input.GetMouseButtonDown(0))
-                {
-                    IsPlay = true;
-                    mov_Val = new Vector3(0, 1, 0);
-                    volumeController.Speed = 40f;
-                }
+              
                 if (IsPlay)
                 {
                     if (volumeController /*&& !mGameOver*/)
-                    {
+                    {                                               
+                        mov_Val = new Vector3(X_POS, 1, 0);
                         if (volumeController.PlayState != CurvyController.CurvyControllerState.Playing) volumeController.Play();
                         Vector2 input = mov_Val; //new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
                                                  // text_s.text = "Playing";
-                        print("input " + input);
+                       // print("input " + input);
                         float speedRaw = volumeController.Speed + input.y * Time.deltaTime * Mathf.Lerp(accelerationBackward, accelerationForward, (input.y + 1f) / 2f);
 
                         volumeController.Speed = Mathf.Clamp(speedRaw, 0f, maxSpeed);
@@ -77,12 +111,7 @@ namespace FluffyUnderware.Curvy.Examples
                         }
                     }
                 }
-                if (Input.GetMouseButtonUp(0))
-                {
-                    IsPlay = false;
-                    mov_Val = new Vector3(0, 0, 0);
-                    StartCoroutine(SpeedTest());
-                }
+               
             }
         }
 
@@ -116,5 +145,93 @@ namespace FluffyUnderware.Curvy.Examples
             ResetController();
             mGameOver = false;
         }
+
+//        private void JumpOrSwipe()
+//        {
+//#if UNITY_EDITOR
+//            if (fingerDown == false && Input.GetMouseButtonDown(0))
+//            {
+//                start_Pos = Input.mousePosition;
+//                fingerDown = true;
+//            }
+//            if (fingerDown)
+//            {
+//                if (Input.mousePosition.y >= start_Pos.y + pixetDistToDetect)
+//                {
+//                    fingerDown = false;
+//                    Jump();
+//                    // Debug.Log("Swipe Up");
+//                }
+//                else if (Input.mousePosition.y <= start_Pos.y - pixetDistToDetect)
+//                {
+//                    fingerDown = false;
+//                    Slide();
+//                    // Debug.Log("Swipe Down");
+//                }
+//            }
+//            if (fingerDown && Input.GetMouseButtonUp(0))
+//            {
+//                fingerDown = false;
+//            }
+//#else
+//        if (fingerDown == false && Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+//        {
+//            start_Pos = Input.touches[0].position;
+//            fingerDown = true;
+//        }
+//        if (fingerDown)
+//        {
+//            if (Input.touches[0].position.y >= start_Pos.y + pixetDistToDetect)
+//            {
+//                fingerDown = false;
+//                Jump();
+//               // ui_Text.text = "Swipe Up";
+//            }
+//            else if (Input.touches[0].position.y <= start_Pos.y - pixetDistToDetect)
+//            {
+//                fingerDown = false;
+//                Slide();
+//               // ui_Text.text = "Swipe Down";
+//            }
+//        }
+//        if (fingerDown && Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Ended)
+//        {
+//            fingerDown = false;
+//        }
+//#endif
+//        }
+//        public void Jump()
+//        {
+//            if (click < 2)
+//            {
+//                isJump = true;
+//                if (click == 0)
+//                {
+//                    audio_Source.enabled = false;
+//                    GameManager.instance.sound_Manager.PlaySound(0);
+//                    this.rb.AddForce(new Vector2(0, jetpackSpeed + 1.2f), ForceMode2D.Impulse);
+//                    StartCoroutine(DoubleJump());
+//                }
+//                else if (click == 1)
+//                {
+//                    if (!isDoubleJump)
+//                    {
+//                        GameManager.instance.sound_Manager.PlaySound(7);
+//                        this.rb.AddForce(new Vector2(0, jetpackSpeed), ForceMode2D.Impulse);
+//                        this.animator.SetBool("DoubleJump", true);
+//                        dbl_Jump_Effect.Play();
+//                    }
+//                }
+//                click++;
+//            }
+//        }
+//        public void Slide()
+//        {
+//            if (!isJump)
+//            {
+//                isSlide = true;
+//                StartCoroutine(SlideOff());
+//            }
+//        }
     }
 }
